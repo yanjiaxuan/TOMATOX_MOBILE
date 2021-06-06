04b041b9cb6f7dece223f4f0aa46b2182e565a61
yanjiaxuan <330544968@qq.com>
2021-06-07 02:51:58 +0800

    + init project

---
+++ android/app/src/main/java/com/tomatox_mobile/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tomatox_mobile;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "TOMATOX_MOBILE";
+  }
+}
