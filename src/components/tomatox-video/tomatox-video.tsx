5f310b6f96503c2e9954358d20e22594ecf7ca37
yanjiaxuan <330544968@qq.com>
2021-06-15 03:57:09 +0800

    player optimize: refactor gesture operation logic and add animation

--- src/components/tomatox-video/tomatox-video.tsx
+++ src/components/tomatox-video/tomatox-video.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Video, {OnLoadData} from 'react-native-video';
 import Orientation, {OrientationType} from 'react-native-orientation-locker';
-import {BackHandler, GestureResponderEvent, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {Animated, BackHandler, GestureResponderEvent, StatusBar, StyleSheet, Text, View} from 'react-native';
 import constants from '../../utils/constants';
 import {convertSecondToTime} from '../../utils/time-converter';
 import SystemSetting from 'react-native-system-setting';
@@ -28,12 +28,15 @@ export default class TomatoxVideo extends React.Component<{
     private touchStartTime = 0
     private videoControlHideTaskId: any
     private touchScreenTaskId: any
-    private posX:any
-    private posY:any
-    private touchMoveCount = 0
+    private initPosX: any
+    private initPosY: any
+    private posX: any
+    private posY: any
     private touchType = -1
     private curShowVideoControl = false
     private selfIsAlive = true
+    private locationInfoAnimatedLeft = new Animated.Value(-200)
+    private controlBarAnimatedOpacity = new Animated.Value(0)
 
     constructor(props: any) {
         super(props);
@@ -71,29 +74,29 @@ export default class TomatoxVideo extends React.Component<{
         if (this.state.showVideoControl) {
             this.videoControlHideTaskId && clearTimeout(this.videoControlHideTaskId);
             this.videoControlHideTaskId = undefined;
-        } else {
-            this.videoControlHideTaskId = this.setLifecycleTimeout(() => {
+            Animated.timing(this.controlBarAnimatedOpacity, { toValue: 0, duration: 300, useNativeDriver: true }).start(() => {
                 this.setState({
                     showVideoControl: false,
                 });
                 this.videoControlHideTaskId = undefined;
+            })
+        } else {
+            this.setState({showVideoControl: true}, () => {
+                Animated.timing(this.controlBarAnimatedOpacity, { toValue: 1, duration: 300, useNativeDriver: true }).start()
+            })
+            this.videoControlHideTaskId = this.setLifecycleTimeout(() => {
+                this.switchControl()
             }, 5000);
         }
-        this.setState({
-            showVideoControl: !this.state.showVideoControl,
-        });
     }
 
     private resetControlShowTimmer = () => {
-        this.setState({
-            showVideoControl: true,
-        });
+        this.setState({showVideoControl: true}, () => {
+            Animated.timing(this.controlBarAnimatedOpacity, { toValue: 1, duration: 300, useNativeDriver: true }).start()
+        })
         this.videoControlHideTaskId && clearTimeout(this.videoControlHideTaskId);
         this.videoControlHideTaskId = this.setLifecycleTimeout(() => {
-            this.setState({
-                showVideoControl: false,
-            });
-            this.videoControlHideTaskId = undefined;
+            this.switchControl();
         }, 5000);
     }
 
@@ -125,67 +128,69 @@ export default class TomatoxVideo extends React.Component<{
         onTouchMove: (event: GestureResponderEvent) => {
             this.resetControlShowTimmer();
             const {pageX, pageY} = event.nativeEvent;
-            const xOffset = pageX - this.posX;
-            const yOffset = pageY - this.posY;
-            if (this.touchMoveCount <= 2) {
-                this.touchMoveCount++;
-                if (this.touchMoveCount === 1) {
-                    this.posX = pageX;
-                    this.posY = pageY;
-                }
-                if (this.touchMoveCount === 2) {
-                    if (Math.abs(xOffset * 2) > Math.abs(yOffset)) {
-                        this.touchType = 1;   // 横向，控制进度
+            const xOffset = pageX - this.posX
+            const yOffset = pageY - this.posY
+            if (this.touchType === -1) {
+                // 判定操作类型
+                const xOffsetAbs = Math.abs(pageX - this.initPosX)
+                const yOffsetAbs = Math.abs(pageY - this.initPosY)
+                console.log(xOffsetAbs, yOffsetAbs)
+                if (xOffsetAbs > 5 || yOffsetAbs > 5) {
+                    if (xOffsetAbs * 2 > yOffsetAbs) {
+                        // 横向，播放进度
+                        this.touchType = 1;
                     } else {
-                        const width = this.state.isFullScreen ? constants.WINDOW_HEIGHT : constants.WINDOW_WIDTH;
+                        const screenWidth = this.state.isFullScreen ? constants.WINDOW_HEIGHT : constants.WINDOW_WIDTH;
                         // 纵向，控制音量和亮度
-                        if (pageX < width / 2) {
+                        if (pageX < screenWidth / 2) {
                             this.touchType = 2;   // 亮度
                         } else {
                             this.touchType = 3;   // 音量
                         }
                     }
                 }
-                return;
-            }
-            switch (this.touchType) {
-                case 1:
-                    this.seeking = true;
-                    const playPos = xOffset > 0 ? Math.min(this.state.videoFullTime, this.state.playPosition + xOffset * 2) :
-                        Math.max(0, this.state.playPosition + xOffset * 2);
-                    this.setState({
-                        playPosition: playPos,
-                        noticeInfo: `${convertSecondToTime(playPos, this.state.videoFullTime)} / ${convertSecondToTime(this.state.videoFullTime, this.state.videoFullTime)}`,
-                    });
-                    break;
-                case 2:
-                    if (yOffset > 0) {
-                        screenBrightness = Math.max(0, screenBrightness - yOffset * 0.1);
-                    } else {
-                        screenBrightness = Math.min(10, screenBrightness - yOffset * 0.1);
-                    }
-                    this.setState({
-                        noticeInfo: `亮度：${Math.floor(screenBrightness * 10)}%`,
-                    });
-                    SystemSetting.setBrightnessForce(screenBrightness);
-                    break;
-                case 3:
-                    if (yOffset > 0) {
-                        // 音量-
+                return
+            } else {
+                // 响应
+                switch (this.touchType) {
+                    case 1:
+                        this.seeking = true;
+                        const playPos = xOffset > 0 ? Math.min(this.state.videoFullTime, this.state.playPosition + xOffset * 2) :
+                            Math.max(0, this.state.playPosition + xOffset * 2);
                         this.setState({
-                            volume: Math.max(0, this.state.volume - yOffset * 0.008),
-                            noticeInfo: `音量：${Math.floor(this.state.volume * 100)}%`,
+                            playPosition: playPos,
+                            noticeInfo: `${convertSecondToTime(playPos, this.state.videoFullTime)} / ${convertSecondToTime(this.state.videoFullTime, this.state.videoFullTime)}`,
                         });
-                    } else {
-                        // 音量+
+                        break;
+                    case 2:
+                        if (yOffset > 0) {
+                            screenBrightness = Math.max(0, screenBrightness - yOffset * 0.1);
+                        } else {
+                            screenBrightness = Math.min(10, screenBrightness - yOffset * 0.1);
+                        }
                         this.setState({
-                            volume: Math.min(1, this.state.volume - yOffset * 0.008),
-                            noticeInfo: `音量：${Math.floor(this.state.volume * 100)}%`,
+                            noticeInfo: `亮度：${Math.floor(screenBrightness * 10)}%`,
                         });
-                    }
-                    break;
-                default:
-                    break;
+                        SystemSetting.setBrightnessForce(screenBrightness);
+                        break;
+                    case 3:
+                        if (yOffset > 0) {
+                            // 音量-
+                            this.setState({
+                                volume: Math.max(0, this.state.volume - yOffset * 0.008),
+                                noticeInfo: `音量：${Math.floor(this.state.volume * 100)}%`,
+                            });
+                        } else {
+                            // 音量+
+                            this.setState({
+                                volume: Math.min(1, this.state.volume - yOffset * 0.008),
+                                noticeInfo: `音量：${Math.floor(this.state.volume * 100)}%`,
+                            });
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
             this.posX = pageX;
             this.posY = pageY;
@@ -193,15 +198,14 @@ export default class TomatoxVideo extends React.Component<{
         onTouchStart: (event: GestureResponderEvent) => {
             this.curShowVideoControl = this.state.showVideoControl;
             const {locationX, locationY} = event.nativeEvent;
-            this.posX = locationX;
-            this.posY = locationY;
+            this.posX = this.initPosX = locationX;
+            // control组件下偏移量45
+            this.posY = this.initPosY = locationY + 45;
             this.touchStartTime = Date.now();
         },
         onTouchEnd: () => {
             const now = Date.now();
-            this.posX = undefined;
-            this.posY = undefined;
-            this.touchMoveCount = 0;
+            this.posX = this.initPosX = this.posY = this.initPosY = undefined;
             this.setState({noticeInfo: ''});
             switch (this.touchType) {
                 case 1:
@@ -209,29 +213,31 @@ export default class TomatoxVideo extends React.Component<{
                     this.setLifecycleTimeout(() => this.seeking = false, 1000);
                     break;
                 case 2:
+                    break;
                 case 3:
+                    break;
                 default:
+                    // 点击
+                    if (this.touchScreenTaskId) {
+                        // 双击 播放/暂停
+                        this.setState({
+                            playing: !this.state.playing,
+                        });
+                        clearTimeout(this.touchScreenTaskId);
+                        this.touchScreenTaskId = undefined;
+                        this.resetControlShowTimmer();
+                    } else {
+                        // 单击，切换控制层状态
+                        this.resetControlShowTimmer()
+                        this.touchScreenTaskId = this.setLifecycleTimeout(() => {
+                            this.curShowVideoControl === this.state.showVideoControl && this.switchControl();
+                            this.touchScreenTaskId = undefined;
+                        },300);
+                    }
                     break;
             }
-            this.touchType = -1;
 
-            if (now - this.touchStartTime < 200) {   // 点击操作
-                if (this.touchScreenTaskId) {
-                    // 双击 播放/暂停
-                    this.setState({
-                        playing: !this.state.playing,
-                    });
-                    clearTimeout(this.touchScreenTaskId);
-                    this.touchScreenTaskId = undefined;
-                    this.resetControlShowTimmer();
-                } else {
-                    // 单击，切换控制层状态
-                    this.touchScreenTaskId = this.setLifecycleTimeout(() => {
-                        this.curShowVideoControl === this.state.showVideoControl && this.switchControl();
-                        this.touchScreenTaskId = undefined;
-                    },500);
-                }
-            }
+            this.touchType = -1;
         },
     }
 
@@ -250,7 +256,16 @@ export default class TomatoxVideo extends React.Component<{
             if (this.props.lastSeek) {
                 this.videoInstance?.seek(this.props.lastSeek)
                 this.setState({locationInfo: `已为您定位至${convertSecondToTime(this.props.lastSeek, this.props.lastSeek)}`})
-                this.setLifecycleTimeout(() => this.setState({locationInfo: ''}), 2500)
+                Animated.timing(this.locationInfoAnimatedLeft, {
+                    toValue: 0,
+                    duration: 500,
+                    useNativeDriver: false
+                }).start()
+                this.setLifecycleTimeout(() => Animated.timing(this.locationInfoAnimatedLeft, {
+                    toValue: -200,
+                    duration: 500,
+                    useNativeDriver: false
+                }).start(), 2500)
             }
         });
     }
@@ -368,38 +383,42 @@ export default class TomatoxVideo extends React.Component<{
                 {
                     this.state.showVideoControl &&
                     <View style={style.videoControl}>
-                        <LinearGradient colors={['rgba(0,0,0,0.7)', 'rgba(0,0,0,0)']} style={style.videoControlTop}>
-                            <TouchableOpacity onPress={this.processGoBack}>
-                                <Icon name={'chevron-left'} style={style.topBtn} />
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                                <Icon name={'airplay'} style={style.topBtnRight} />
-                            </TouchableOpacity>
-                        </LinearGradient>
-                        <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.7)']}
-                                        style={style.videoControlBottom}>
-                            <TouchableOpacity onPress={() => {this.resetControlShowTimmer(); this.setState({ playing: !this.state.playing });}}>
-                                <Icon name={this.state.playing ? 'pause' : 'play'} style={style.videoPlayPause}/>
-                            </TouchableOpacity>
-                            <Slider
-                                style={style.videoSlider}
-                                value={this.state.playPosition}
-                                minimumValue={0}
-                                maximumValue={this.state.videoFullTime}
-                                minimumTrackTintColor={TOMATOX_THEME.THEME_COLOR}
-                                maximumTrackTintColor={TOMATOX_THEME.UNIMPORTANT_FONT_COLOR}
-                                thumbTintColor={TOMATOX_THEME.THEME_COLOR}
-                                onTouchStart={() => this.seeking = true}
-                                onSlidingComplete={() => {this.videoInstance?.seek(this.curTimeCache); this.setLifecycleTimeout(() => this.seeking = false, 1000);}}
-                                onValueChange={value => {this.curTimeCache = value; this.setState({playPosition: value}); this.resetControlShowTimmer();}}
-                            />
-                            <Text style={style.videoProcess}>
-                                {`${convertSecondToTime(this.state.playPosition, this.state.videoFullTime)}/${convertSecondToTime(this.state.videoFullTime, this.state.videoFullTime)}`}
-                            </Text>
-                            <TouchableOpacity onPress={this.switchScreenState}>
-                                <Icon name={this.state.isFullScreen ? 'minimize' : 'maximize'} style={style.videoFullScreen} />
-                            </TouchableOpacity>
-                        </LinearGradient>
+                        <Animated.View style={{opacity: this.controlBarAnimatedOpacity}}>
+                            <LinearGradient colors={['rgba(0,0,0,0.7)', 'rgba(0,0,0,0)']} style={style.videoControlTop}>
+                                <TouchableOpacity onPress={this.processGoBack}>
+                                    <Icon name={'chevron-left'} style={style.topBtn} />
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                    <Icon name={'airplay'} style={style.topBtnRight} />
+                                </TouchableOpacity>
+                            </LinearGradient>
+                        </Animated.View>
+                        <Animated.View style={{opacity: this.controlBarAnimatedOpacity}}>
+                            <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.7)']}
+                                            style={style.videoControlBottom}>
+                                <TouchableOpacity onPress={() => {this.resetControlShowTimmer(); this.setState({ playing: !this.state.playing });}}>
+                                    <Icon name={this.state.playing ? 'pause' : 'play'} style={style.videoPlayPause}/>
+                                </TouchableOpacity>
+                                <Slider
+                                    style={style.videoSlider}
+                                    value={this.state.playPosition}
+                                    minimumValue={0}
+                                    maximumValue={this.state.videoFullTime}
+                                    minimumTrackTintColor={TOMATOX_THEME.THEME_COLOR}
+                                    maximumTrackTintColor={TOMATOX_THEME.UNIMPORTANT_FONT_COLOR}
+                                    thumbTintColor={TOMATOX_THEME.THEME_COLOR}
+                                    onTouchStart={() => this.seeking = true}
+                                    onSlidingComplete={() => {this.videoInstance?.seek(this.curTimeCache); this.setLifecycleTimeout(() => this.seeking = false, 1000);}}
+                                    onValueChange={value => {this.curTimeCache = value; this.setState({playPosition: value}); this.resetControlShowTimmer();}}
+                                />
+                                <Text style={style.videoProcess}>
+                                    {`${convertSecondToTime(this.state.playPosition, this.state.videoFullTime)}/${convertSecondToTime(this.state.videoFullTime, this.state.videoFullTime)}`}
+                                </Text>
+                                <TouchableOpacity onPress={this.switchScreenState}>
+                                    <Icon name={this.state.isFullScreen ? 'minimize' : 'maximize'} style={style.videoFullScreen} />
+                                </TouchableOpacity>
+                            </LinearGradient>
+                        </Animated.View>
                     </View>
                 }
                 <View
@@ -411,7 +430,9 @@ export default class TomatoxVideo extends React.Component<{
                         <Text style={style.videoControlCenterContent}>{this.state.noticeInfo || this.state.bufferingInfo}</Text>
                     }
                 </View>
-                <Text style={style.locationInfoContent}>{this.state.locationInfo}</Text>
+                <Animated.View style={{left: this.locationInfoAnimatedLeft}}>
+                    <Text style={style.locationInfoContent}>{this.state.locationInfo}</Text>
+                </Animated.View>
                 <Video
                     ref={(instance: Video) => this.videoInstance = instance}
                     onLoad={this.onVideoLoad} // when loaded, record fullTime
